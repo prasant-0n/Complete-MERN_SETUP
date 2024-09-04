23dfd790ad51269cc0be408e7134167ff1d8d622
prasanta kumar sahoo <prasanta6212@gmail.com>
2024-09-05 01:15:57 +0530

    mern setup completed

---
+++ frontend/src/App.tsx
@@ -0,0 +1,13 @@
+import './App.css';
+import First from './components/First';
+
+function App() {
+  return (
+    <div className="">
+      <h1 className="text-3xl font-bold underline">Hello world!</h1>
+      <First />
+    </div>
+  );
+}
+
+export default App;
